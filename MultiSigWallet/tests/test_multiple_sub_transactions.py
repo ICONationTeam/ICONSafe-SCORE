1d8e69e91782682699b77df6f61d9ef0c1376fe4
Spl3en <spl3en.contact@gmail.com>
2020-08-29 15:54:04 +0200

    Add failing test_multiple_sub_transactions_must_be_atomic

--- MultiSigWallet/tests/test_multiple_sub_transactions.py
+++ MultiSigWallet/tests/test_multiple_sub_transactions.py
@@ -59,3 +59,32 @@ class TestIntegrateSubmitTransaction(MultiSigWalletTests):
         # Check new updated IRC2 balance
         new_irc2_balance = self.balance_token(self._user.get_address())
         self.assertEqual(new_irc2_balance, initial_irc2_balance + 1500)
+
+    def test_multiple_sub_transactions_must_be_atomic(self):
+        self.deposit_icx_to_multisig_score(10000)
+
+        result = self.set_wallet_owners_required(2)
+        result = self.confirm_transaction_created(result)
+        txuid = self.get_transaction_execution_success_uid(result)
+        self.assertEqual("EXECUTED", self.get_transaction(txuid)['state'])
+
+        initial_icx_balance = get_icx_balance(super(), str(self._user.get_address()), self.icon_service)
+
+        # The 2 first transactions should work, but the third one raises an out of balance
+        # exception. All transactions must be rollbacked.
+        sub_transactions = []
+        sub_transactions.append(self.msw_transfer_icx_params(self._user.get_address(), 1000))
+        sub_transactions.append(self.msw_transfer_icx_params(self._user.get_address(), 2000))
+        sub_transactions.append(self.msw_transfer_icx_params(self._user.get_address(), 10000000))
+
+        result = self.submit_transaction(self._operator, sub_transactions)
+        txuid = self.get_transaction_created_uid(result)
+
+        # Check if transaction failed
+        result = self.confirm_transaction(txuid, self._operator)
+        result = self.confirm_transaction(txuid, self._owner2)
+        self.assertEqual("FAILED", self.get_transaction(txuid)['state'])
+
+        # Check if rollbacked
+        new_icx_balance = get_icx_balance(super(), str(self._user.get_address()), self.icon_service)
+        self.assertEqual(new_icx_balance, initial_icx_balance)
